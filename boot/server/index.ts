9a45c18152fcbda642c321b99b447b572fed1666
Guilherme Bromonschenkel <guilhermebromonschenkel@gmail.com>
2019-10-29 21:58:47 -0300

    feat(Server) Add server

---
+++ boot/server/index.ts
@@ -0,0 +1,25 @@
+import express from "express"
+import { Express } from "express"
+
+import Middlewares from "boot/server/middlewares"
+
+class Server {
+	private app: Express = express()
+
+	public async start() {
+		this.setupMiddlewares()
+		this.initServer()
+	}
+
+	private setupMiddlewares() {
+		Middlewares.map(middleware => this.app.use(middleware))
+	}
+
+	private initServer() {
+		this.app.listen(process.env.PORT, () => {
+			console.log(`Server running... [PORT ${process.env.PORT}]`)
+		})
+	}
+}
+
+export default new Server()
---
+++ boot/server/middlewares.ts
@@ -0,0 +1,5 @@
+import express from "express"
+
+const Middlewares = [express.json()]
+
+export default Middlewares
