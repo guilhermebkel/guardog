c24a9de122c01aea3a78d208348514ba5521fee8
Guilherme <guilhermebromonschenkel@gmail.com>
2019-11-02 06:56:53 -0300

    feat(view) Add front end structure and page rendering

--- boot/server/index.ts
+++ boot/server/index.ts
@@ -1,7 +1,9 @@
 import express from "express"
+import * as path from "path"
+import * as ejs from "ejs"
 import { Express } from "express"
 
-import Middlewares from "boot/server/middlewares"
+import middlewares from "boot/server/middlewares"
 import routes from "~/routes"
 
 class Server {
@@ -10,15 +12,32 @@ class Server {
 	async start() {
 		this.setupMiddlewares()
 		this.setupRoutes()
+		this.setupStaticFiles()
+		this.renderStaticFiles()
 		this.initServer()
 	}
 
 	setupMiddlewares() {
-		Middlewares.map(middleware => this.app.use(middleware))
+		middlewares.map(middleware => this.app.use(middleware))
 	}
 
 	setupRoutes() {
 		routes.map(route => this.app.use(route))
+	}
+
+	setupStaticFiles() {
+		this.app.use(
+			express.static(path.join(__dirname, "..", "..", "..", "public"))
+		)
+		this.app.set("views", path.join(__dirname, "..", "..", "..", "public"))
+		this.app.engine("html", ejs.renderFile)
+		this.app.set("view engine", "html")
+	}
+
+	renderStaticFiles() {
+		this.app.use("/", (req, res) => {
+			res.render("index.html")
+		})
 	}
 
 	initServer() {
--- boot/server/middlewares.ts
+++ boot/server/middlewares.ts
@@ -2,6 +2,6 @@ import express from "express"
 import cors from "cors"
 import helmet from "helmet"
 
-const Middlewares = [express.json(), cors(), helmet()]
+const middlewares = [express.json(), cors(), helmet()]
 
-export default Middlewares
+export default middlewares
